aa3dc7e508e6375223acdecc367ef1c38cc7f3ba
rossjbm <118684575+rossjbm@users.noreply.github.com>
2024-03-04 23:21:06 -0400

    Opiniones de Usuarios

--- FRONTEND/src/paginas/Inicio/Inicio.jsx
+++ FRONTEND/src/paginas/Inicio/Inicio.jsx
@@ -1,6 +1,6 @@
 
 import { ServicioCarrusel } from "../../componentes/Inicio/ServicioCarrusel";
-
+import { Horarios } from "../../componentes/Inicio/Horarios";
 
 export function Inicio() {
     return (<>
@@ -24,36 +24,7 @@ export function Inicio() {
             </div>
 
             <div className="font-textos grid grid-cols-1 md:grid-cols-2 xl:grid-cols-3 gap-11 p-10">
-                <section className="p-5 border-black border-2 flex flex-col gap-4 justify-start items-center">
-                    <h4 className="text-xl font-semibold">Atención al Cliente</h4>
-                    <p className="text-lg font-bold bg-blue-200 px-2 py-1">24 horas del día</p>
-                    <p className="text-center text-xl">Nuestro personal de recepción está siempre disponible para ayudarte con cualquier necesidad que tengas durante tu estancia</p>
-                </section>
-                <section className="p-5 border-black border-2 flex flex-col gap-4 justify-start items-center">
-                    <h4 className="text-xl font-semibold">Restaurante Gourmet</h4>
-                    <p className="text-lg font-bold bg-blue-200 px-2 py-1">7:00 a.m a 10:00 p.m</p>
-                    <p className="text-center text-xl">Comienza tu día con un desayuno buffet, y puedes disfrutar todo el día de servicios al cuarto</p>
-                </section>
-                <section className="p-5 border-black border-2 flex flex-col gap-4 justify-star items-center">
-                    <h4 className="text-xl font-semibold">Spa de Relajación</h4>
-                    <p className="text-lg font-bold bg-blue-200 px-2 py-1">9:00 a.m a 6:00 p.m</p>
-                    <p className="text-center text-xl">Te recomendamos hacer una reserva del spa con anticipación para disfrutar tu día de relajación</p>
-                </section>
-                <section className="p-5 border-black border-2 flex flex-col gap-4 justify-start items-center">
-                    <h4 className="text-xl font-semibold">Piscina</h4>
-                    <p className="text-lg font-bold bg-blue-200 px-2 py-1">6:00 a.m a 10:00 p.m</p>
-                    <p className="text-center text-xl">Nuestra piscina al aire libre es el lugar perfecto para relajarse y disfrutar del sol desde temprano</p>
-                </section>
-                <section className="p-5 border-black border-2 flex flex-col gap-4 justify-start items-center">
-                    <h4 className="text-xl font-semibold">Actividades Recreativas</h4>
-                    <p className="text-lg font-bold bg-blue-200 px-2 py-1">Depende de la Actividad</p>
-                    <p className="text-center text-xl">Las actividades programadas, como deportes acuáticos y excursiones, generalmente se realizan durante el día</p>
-                </section>
-                <section className="p-5 border-black border-2 flex flex-col gap-4 justify-start items-center">
-                    <h4 className="text-xl font-semibold">Gimnasio</h4>
-                    <p className="text-lg font-bold bg-blue-200 px-2 py-1">24 horas del día</p>
-                    <p className="text-center text-xl">Nuestro gimnasio está disponible para los huéspedes que deseen mantener su rutina de ejercicios durante su estancia</p>
-                </section>
+                <Horarios/>
             </div>
 
             <div className="static h-36 bg-bgFondo2 bg-cover bg-fixed w-full ">
--- FRONTEND/src/paginas/Landing/Landing.jsx
+++ FRONTEND/src/paginas/Landing/Landing.jsx
@@ -1,12 +1,10 @@
 import { Link } from 'react-router-dom';
 import { useEffect } from 'react';
 
-
 import { Parallax } from '../../componentes/Parallax/Parallax';
-import ImgUbicacion from '../../assets/ubicacion2.jpg';
-import ImgSotisficado from '../../assets/sotisficado2.jpeg';
-import ImgGastronomia from '../../assets/gastronomia2.jpg';
-import ImgActividades from '../../assets/actividades2.jpg';
+import { Servicios } from '../../componentes/Landing/Servicios';
+import { Opiniones } from '../../componentes/Landing/Opiniones';
+
 
 export function Landing({setHeaderMostrar, setBotonRMostrar}) {
     //NO MOSTRAR HEADER
@@ -40,50 +38,16 @@ export function Landing({setHeaderMostrar, setBotonRMostrar}) {
                 <section className='flex flex-col items-center gap-20'>
                     <h3 className='font-bold text-2xl font-textos text-justify md:text-3xl'>Te Ofrecemos...</h3>
 
-                    <div className='grid grid-cols-1 gap-32 xl:grid-cols-2 xl:gap-20 xl:m-10'>
-                        <section className='flex flex-col sm:flex-row gap-5 bg-gray-300 shadow-2xl'>
-                            <div className='h-80 xl:h-full sm:w-1/2 shadow-2xl'>
-                                <img src={ImgUbicacion} alt="Ubicación de HazbinHotel" className='w-full h-full object-cover'/>
-                            </div>
-                            <div className='sm:w-1/2 p-5 flex flex-col gap-6'>
-                                <h4 className='text-2xl font-textos font-semibold text-white'> Ubicación Perfecta</h4>
-                                <p className='text-lg font-textos text-justify text-white'>Situado en la pintoresca Isla de Margarita, nuestro hotel ofrece vistas panorámicas al mar Caribe y acceso directo a las playas de aguas cristalinas. ¡Despiértate con el sonido de las olas!</p>
-                            </div> 
-                        </section>
-                        <section className='flex flex-col sm:flex-row gap-5 bg-gray-300 shadow-2xl'>
-                            <div className='h-80 xl:h-full sm:w-1/2 shadow-2xl '>
-                                <img src={ImgSotisficado} alt="Diseño Interior de HazbinHotel" className='w-full h-full object-cover'/>
-                            </div>
-                            <div className='sm:w-1/2 p-5 flex flex-col gap-6'>
-                                <h4 className='text-2xl font-textos font-semibold text-white'>Diseño Sofisticado</h4>
-                                <p className='text-lg font-textos text-justify text-white'>Cada rincón de HazbinHotel ha sido diseñado pensando en la comodidad y el lujo. Nuestras habitaciones y suites cuentan con mobiliario elegante y colores relajantes.</p>
-                            </div>
-                        </section>
-                        <section className='flex flex-col sm:flex-row gap-5 bg-gray-300 shadow-2xl'>
-                            <div className='h-80 xl:h-full sm:w-1/2 shadow-2xl '>
-                                <img src={ImgGastronomia} alt="Comidas en HazbinHotel" className='w-full h-full object-cover'/>
-                            </div>
-                            <div className='sm:w-1/2 p-5 flex flex-col gap-6'>
-                                <h4 className='text-2xl font-textos font-semibold text-white'>Experiencias Gastronómicas</h4>
-                                <p className='text-lg font-textos text-justify text-white'>Nuestro restaurante ofrece una fusión de sabores locales e internacionales. Disfruta de platos frescos y deliciosos mientras contemplas las vistas al mar.</p>
-                            </div>
-                        </section> 
-
-                        <section className='flex flex-col sm:flex-row gap-5 bg-gray-300 shadow-2xl'>
-                            <div className='h-80 xl:h-full sm:w-1/2 shadow-2xl '>
-                                <img src={ImgActividades} alt="Actividades en HazbinHotel" className='w-full h-full object-cover'/>
-                            </div>
-                            <div className='sm:w-1/2 p-5 flex flex-col gap-6'>
-                                <h4 className='text-2xl font-textos font-semibold text-white'>Actividades para Todos</h4>
-                                <p className='text-lg font-textos text-justify text-white'>Desde deportes acuáticos hasta excursiones por la isla, tenemos opciones para todos los gustos. Explora la belleza natural de Margarita o simplemente relájate en la playa.</p>
-                            </div>
-                        </section>
-                    </div>
+                    <Servicios/>
                 </section>
             </article>
         </div>
         <div className='bg-landingFondo1 bg-cover bg-fixed shadow-2xl w-full'>
             <section className='bg-gray-300 flex flex-col items-center justify-center gap-16 py-20'>
+                <h3 className='text-white font-bold text-2xl font-textos text-center w-4/5 sm:text-3xl'>Usuarios opinan que...</h3>
+                <section className='grid grid-cols-1 px-20 md:px-10 md:grid-cols-3 gap-10 font-textos '>
+                    <Opiniones/>
+                </section>
                 <h3 className='text-white font-bold text-2xl font-textos text-center w-4/5 sm:text-3xl'>¿Qué estás esperando?</h3>
                 <Link to={`inicio`} className='w-4/5 flex flex-col items-center no-underline hover:text-black'>
                     <button className='w-full h-16 border-white border-2 no-underline text-white text-xl font-textos md:w-1/2 hover:bg-black'>Ingresa Ahora</button>
