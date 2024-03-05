e7c6dd801e273db02ef2890341e99fa1b6e08ee5
rossjbm <118684575+rossjbm@users.noreply.github.com>
2024-03-05 02:06:47 -0400

    Contacto y Pago

--- FRONTEND/src/App.jsx
+++ FRONTEND/src/App.jsx
@@ -4,12 +4,12 @@ import { Landing } from "./paginas/Landing/Landing";
 import { Encabezado } from "./componentes/Encabezado/Encabezado";
 import { Ofertas } from "./paginas/Ofertas/Ofertas";
 // import { Reservar } from "./paginas/Reservar/Reservar";
-// import { Acceder } from "./paginas/IniciarSesion/Acceder";
 import { Error } from "./paginas/NotFound/Error";
 import { BotonReservar } from "./componentes/botones/BotonReservar";
 import { Pie } from "./componentes/PiePagina/Pie";
 import { Habitaciones } from "./paginas/Habitaciones/Habitaciones";
-
+import { Contacto } from "./paginas/Contacto/Contacto";
+import { Pago } from "./paginas/Pago/Pago";
 
 // REACT ROUTER
 import { Routes, Route} from "react-router-dom";
@@ -26,12 +26,11 @@ export function App() {
             <Routes>
                 <Route path="/" element={<Landing setHeaderMostrar={setHeaderMostrar} setBotonRMostrar={setBotonRMostrar}/>} />
                 <Route path="/inicio" element={<Inicio/>} />
-                <Route path="/acceder" element={<h1>ESTAMOS EN INICIAR SESION</h1>} />
                 <Route path="/ofertas" element={<Ofertas/>} />
                 <Route path="/habitaciones" element={<Habitaciones/>} />
                 <Route path="/blog" element={<h1>ESTAMOS EN BLOG</h1>} />
-                <Route path="/pagos" element={<h1>ESTAMOS EN MODOS DE PAGOS</h1>} />
-                <Route path="/contacto" element={<h1>ESTAMOS EN CONTACTO</h1>} />
+                <Route path="/pagos" element={<Pago/>} />
+                <Route path="/contacto" element={<Contacto/>} />
                 <Route path="/reservar" element={<h1>ESTAMOS EN RESERVAR, recuerda no mostrar boton</h1>} />
                 <Route path="*" element={<Error setHeaderMostrar={setHeaderMostrar} setBotonRMostrar={setBotonRMostrar} setPieMostrar={setPieMostrar}/>} />
             </Routes>
